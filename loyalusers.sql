5684dd872db178005021ed8c9a6e42b6e664fb86
Rohit2350 <77195677+Rohit2350@users.noreply.github.com>
2024-09-21 13:56:41 +0530

    Add files via upload

---
+++ loyalusers.sql
@@ -0,0 +1,65 @@
+create database ig_users;
+USE ig_users;
+
+/*Users*/
+CREATE TABLE users(
+	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	username VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+/*Photos*/
+CREATE TABLE photos(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	image_url VARCHAR(355) NOT NULL,
+	user_id INT NOT NULL,
+	created_dat TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+/*Comments*/
+CREATE TABLE comments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+/*Likes*/
+CREATE TABLE likes(
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	PRIMARY KEY(user_id,photo_id)
+);
+
+/*follows*/
+CREATE TABLE follows(
+	follower_id INT NOT NULL,
+	followee_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (follower_id) REFERENCES users(id),
+	FOREIGN KEY (followee_id) REFERENCES users(id),
+	PRIMARY KEY(follower_id,followee_id)
+);
+
+/*Tags*/
+CREATE TABLE tags(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	tag_name VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+/*junction table: Photos - Tags*/
+CREATE TABLE photo_tags(
+	photo_id INT NOT NULL,
+	tag_id INT NOT NULL,
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	FOREIGN KEY(tag_id) REFERENCES tags(id),
+	PRIMARY KEY(photo_id,tag_id)
+);
